678f05a851f2c31a8608da886c335aa33c7ad03d
Anass <146187264+nasaou@users.noreply.github.com>
2025-11-09 16:21:35 +0100

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'שקד ירושלמי סחר בנשק',
+  description: 'שקד ירושלמי הוא איש יוצא שירות ביטחון כללי, סוחר נשק חוקי בין מדינות ומגשר בין ארגונים שמוכרים וקונים נשקים ואמל״ח.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="he" dir="rtl">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </head>
+      <body className={inter.className}>
+        <div className="flex min-h-screen flex-col">
+          {/* Navbar will be inserted here */}
+
+          {children}
+
+        </div>
+      </body>
+    </html>
+  );
+}
